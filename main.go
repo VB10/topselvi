f80f55fbd7e88f74dbc9c1343069b98053ee1686
Veli <Bacik>
2019-09-03 21:39:37 +0300

    firebase db admin sdk okey

--- main.go
+++ main.go
@@ -11,14 +11,45 @@ import (
 	"google.golang.org/api/option"
 )
 
+type Book struct {
+	Title  string `json:title`
+	Author string `json:author`
+}
+
 func init() {
-	// firebase init
-	opt := option.WithCredentialsFile("path/to/refreshToken.json")
-	config := &firebase.Config{ProjectID: "my-project-id"}
+
+	opt := option.WithCredentialsFile("assets/you2win.json")
+	config := &firebase.Config{ProjectID: "you2win-3b9d9", DatabaseURL: "https://you2win-3b9d9.firebaseio.com/"}
 	app, err := firebase.NewApp(context.Background(), config, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
+
+	defaultDatabase, err := app.Database(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+
+	}
+	_book := Book{Title: "Veli", Author: "Bacik"}
+	data, err := defaultDatabase.NewRef("test").Child("testingo").Push(context.Background(), _book)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+	print(data.Path)
+
+	// defaultClient, err := app.Auth(context.Background())
+	// if err != nil {
+	// 	log.Fatalf("error getting Auth client: %v\n", err)
+	// }
+
+	// user, err := defaultClient.GetUserByEmail(context.Background(), "velibacik@gmail.com")
+	// if err != nil {
+	// 	log.Fatalf("error getting Auth client: %v\n", err)
+
+	// }
+
+	// print(user.Email)
+
 }
 
 func refreshToken(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +57,7 @@ func refreshToken(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+
+	// http.HandleFunc("/", greet)
+	// http.ListenAndServe(":8080", nil)
 }
