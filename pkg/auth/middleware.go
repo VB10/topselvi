21e3688315238251d1ed547c449142fd13ec6010
Veli <Bacik>
2019-09-10 22:41:28 +0300

    last changes

---
+++ pkg/auth/middleware.go
@@ -0,0 +1,31 @@
+package auth
+
+import (
+	"io"
+	"net/http"
+)
+
+// Middleware (this function) makes adding more than one layer of middleware easy
+// by specifying them as a list. It will run the last specified handler first.
+func Middleware(h http.Handler, middleware ...func(http.Handler) http.Handler) http.Handler {
+	for _, mw := range middleware {
+		h = mw(h)
+	}
+	return h
+}
+
+// AuthMiddleware is an example of a middleware layer. It handles the request authorization
+// by checking for a key in the url.
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestKey := r.Header.Get("token")
+		if len(requestKey) == 0 {
+			// Report Unauthorized
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			io.WriteString(w, `{"error":"invalid_key"}`)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
